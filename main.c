3118349fefe9da0690e12a43dbe76beb8b5d994d
gneve <gregoire.neve@gmail.com>
2021-05-15 21:35:36 +0000

    gregoire

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 int main(int argc, char **argv)
 {
     int i = 0;
+    char buffer[1000000];
+    int co;
     while (i < argc)
     {
-        printf("%s\n", argv[i]);
+        FILE* filedata = fopen(argv[i], "r");
+        int co = fread(&buffer, sizeof(char), 1000000, filedata);
+        parsert(buffer);
+        co = 0;
         i++;
     }
+    return 0;
 }
---
+++ parser.c
@@ -0,0 +1,6 @@
+#include "lib/default.h"
+
+char parsert(char *buffer)
+{
+    printf("Data read from file: %s \n", buffer);
+}
